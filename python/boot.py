9ed14a77561f808df37cda2a2153f8881b3df545
Gilaxy04 <79372051+franck403@users.noreply.github.com>
2026-08-31 06:57:22 -0400

    Disable Bluetooth initialization in boot.py
    Comment out Bluetooth initialization and exception handling.

--- python/boot.py
+++ python/boot.py
@@ -1,20 +1,20 @@
 import esp32
 
 print("at boot:", esp32.idf_heap_info(esp32.HEAP_DATA))
+#
+#import bluetooth
+#import machine
+#import time
 
-import bluetooth
-import machine
-import time
+#ble = bluetooth.BLE()
 
-ble = bluetooth.BLE()
-
-try:
-    ble.active(False)
-    time.sleep_ms(100)
-except Exception:
-    pass
-
-try:
-    ble.active(True)
-except Exception:
-    machine.reset()
+#try:
+#    ble.active(False)
+#    time.sleep_ms(100)
+#except Exception:
+#    pass
+#
+#try:
+#    ble.active(True)
+#except Exception:
+#    machine.reset()
