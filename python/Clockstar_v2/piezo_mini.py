e899a65cba5a2eaf78c197b2056a1873a5c26875
Gilaxy04 <79372051+franck403@users.noreply.github.com>
2026-09-01 09:15:13 -0400

    remove piezo for security of no sound

--- python/Clockstar_v2/piezo_mini.py
+++ python/Clockstar_v2/piezo_mini.py
@@ -1,12 +1,13 @@
 import time
 from machine import Pin, PWM
 
-
+# disabled
 class Piezo:
     def __init__(self, pin):
         self._pin = Pin(pin, mode=Pin.OUT)
 
     def tone(self, freq, duration_ms):
+        return
         pwm = PWM(self._pin, freq=freq, duty_u16=32768)
         time.sleep_ms(duration_ms)
         pwm.deinit()
